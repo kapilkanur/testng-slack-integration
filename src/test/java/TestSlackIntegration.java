7fa4b43f7f0fb6b288722d859f03c224fa1a19fa
Kapil Kanur <kanurkapil@gmail.com>
2021-07-16 13:29:34 +0530

    Added Testng, Log4j dependency, Test Case
    Added:
    - TestNG,
    - slf4j dependency
    - Test case class

---
+++ src/test/java/TestSlackIntegration.java
@@ -0,0 +1,34 @@
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.testng.Assert;
+import org.testng.SkipException;
+import org.testng.annotations.Test;
+
+public class TestSlackIntegration {
+
+    private static final Logger LOG = LoggerFactory.getLogger(TestSlackIntegration.class);
+
+    @Test
+    public void caseThatPasses() {
+        LOG.info("Test case passed");
+        Assert.assertTrue(true);
+    }
+
+    @Test
+    public void caseThatPasses2() {
+        LOG.info("Test case passed2");
+        Assert.assertTrue(true);
+    }
+
+    @Test
+    public void caseThatFails() {
+        LOG.info("Test case failed");
+        Assert.assertTrue(false);
+    }
+
+    @Test
+    public void caseThatGetsSkipped() {
+        LOG.info("Test case skipped");
+        throw new SkipException("Skipping this case");
+    }
+}
